b05cfdc5c718cf06f3cace37c80427d30cc61eb8
deekondabhanuteja <deekondabhanuteja@gmail.com>
2021-06-05 09:59:32 +0530

    MangaOnlineProject structure.

---
+++ Service/readmangaonline/src/test/java/com/manga/readmangaonline/ReadmangaonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manga.readmangaonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadmangaonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
